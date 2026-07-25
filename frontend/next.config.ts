7b3ab121e5aa51424bf80a0696f969c12c4c22e7
Gilang Abdian Anggara <gilangabdian21@gmail.com>
2026-07-25 16:45:29 +0700

    fix:force sitemap (#65)

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,8 +5,19 @@ const nextConfig: any = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  staticPageGenerationTimeout: 300, // Menunggu maksimal 5 menit untuk Render bangun
+  async headers() {
+    return [
+      {
+        source: "/sitemap.xml",
+        headers: [
+          { key: "X-Robots-Tag", value: "index, follow" },
+          { key: "Content-Type", value: "application/xml" },
+        ],
+      },
+    ];
+  },
 
+  staticPageGenerationTimeout: 300, // Menunggu maksimal 5 menit untuk Render bangun
 };
 
 export default nextConfig;
